e8ae7bd2455bde6df02116b4d616fc3ca47200a5
Ganesh0412 <33178308+Ganesh0412@users.noreply.github.com>
2026-04-27 05:54:06 +0000

    test

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -67,4 +67,15 @@ def test_get_nth_fibonacci_one():
 #     result = get_nth_fibonacci(n)
 
 #     # Assert
-#     assert result == 89
+#     assert result == 89   
+
+def test_get_nth_fibonacci_ten():
+ """Test with n=10."""
+ # Arrange
+ n = 10
+
+ # Act
+ result = get_nth_fibonacci(n)
+
+ # Assert
+ assert result == 89
